e83a5d3c3afdee99e686f457761d02400273077e
Daniel Joey Cheung <dcheun47@uwo.ca>
2021-05-11 16:35:09 -0400

    Added basic CRUD functionality to site Can now create, read, update and delete spots

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const app = express();
 const path = require('path');
 const Spots = require('./models/spots');
+const methodOverride = require('method-override');
 
 //Connecting mongoose to mongoDB
 mongoose.connect('mongodb://localhost:27017/darkslide-spots',{
@@ -21,23 +22,61 @@ db.once("open",() => {
 //Set view engine as ejs, so express knows we are using ejs files for views
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
 
 //GET request for home @url/
 app.get('/', (req, res)=>{
     res.render('home')
 })
 
-//GET request for new spot form, @url/newSpot
-app.get('/newSpot', async (req,res)=>{
-    const spot = new Spots({title: 'Local Skate Test'})
-    await spot.save();
-    res.send(spot);
+//GET request for new spot form, @url/spots/newSpot
+app.get('/spots/newSpot', async (req,res)=>{
+    res.render('spots/new');
 })
 
-//Catch for GET request if no route has yet been made
+//POST request for new spot form, redirects to new spot detail page on send
+app.post('/spots/newSpot', async(req, res) => {
+    const spot = new Spots (req.body.spots);
+    await spot.save();
+    res.redirect(`/spots/${spot._id}`);
+})
+
+//GET request of all spots on database
+app.get('/spots', async(req, res)=>{
+    const spots = await Spots.find({});
+    res.render('spots/index', {spots});
+})
+
+//GET request for specific spot ID in database, renders show page for requested ID
+app.get('/spots/:id', async(req, res) =>{
+    const spots = await Spots.findById(req.params.id);
+    res.render('spots/detail', {spots});
+})
+
+//GET request to edit spot of requested ID
+app.get('/spots/:id/edit', async(req, res) =>{
+    const spots = await Spots.findById(req.params.id);
+    res.render('spots/edit', {spots});
+})
+
+app.put('/spots/:id', async(req,res) =>{
+    const {id} = req.params;
+    const spots = await Spots.findByIdAndUpdate(id, {...req.body.spots});
+    res.redirect(`/spots/${spots._id}`);
+
+})
+
+app.delete('/spots/:id', async(req, res) => { 
+    const {id} = req.params;
+    Spots.findByIdAndDelete(id);
+    res.redirect('/spots')
+})
+
+//Catch for GET request if no route has been made yet
 app.get('*', (req,res) =>{ res.send('We are still working on this part of the site.') })
 
 //Listen on port 3000
-app.listen(3000), ()=> {  
+app.listen(3000, ()=> {  
     console.log('serving on port 3000')
-}
+})
